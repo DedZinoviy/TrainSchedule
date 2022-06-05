e4d813ec8e4c4bcb83e41bd10b2e042b6c6a40b3
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-05 13:21:02 +0300

    Added abstract repository interface.

---
+++ TrainSchedule/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainSchedule.RepositoryInterfaces
+{
+    /// <summary>
+    /// Interface for repositories
+    /// </summary>
+    /// <typeparam name="T">Data type to create repository for</typeparam>
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// Allows get object by ID
+        /// </summary>
+        /// <param name="id">ID of object to get</param>
+        /// <returns>Recieved object</returns>
+        T getById(uint id);
+    }
+}
