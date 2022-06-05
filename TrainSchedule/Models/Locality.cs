1da3b4a4d614d9d3068f8bcc56aa1a60a3572bcb
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-05 13:05:37 +0300

    Added a class for describing localities.

---
+++ TrainSchedule/Models/Locality.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainSchedule.Models
+{
+    /// <summary>
+    /// Class for describing localities.
+    /// </summary>
+    public class Locality
+    {
+        ///<value>Id of Locality</value>
+        public uint Id { get; set; }
+
+        ///<value>Name of locality</value> 
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="id">Id of Locality to be created</param>
+        /// <param name="name">Name of Locality to be created</param>
+        public Locality(uint id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
