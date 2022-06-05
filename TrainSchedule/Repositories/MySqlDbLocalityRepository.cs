e6d01f278d3c9913eb8342274a13e564efc85b54
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-05 14:38:42 +0300

    Started the implementation of the Locality repository for MySQL .

---
+++ TrainSchedule/Repositories/MySqlDbLocalityRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainSchedule.Models;
+using TrainSchedule.RepositoryInterfaces;
+
+namespace TrainSchedule.Repositories
+{
+    public class MySqlDbLocalityRepository : ILocalityRepository
+    {
+        public Locality append(Locality newLocality)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Locality update(Locality locality)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Locality getByName(string name)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void delete(Locality locality)
+        {
+
+        }
+
+        public Locality getById(uint id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
