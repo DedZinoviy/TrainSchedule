dc99b2c1104070a272c126de52f188a7245911ef
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-06 13:41:24 +0300

    The class of connections to a DB is created. Implemented a method to connect to MySQL.

---
+++ TrainSchedule/Conections/ConnectUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainSchedule.Conections
+{
+    /// <summary>
+    /// A class containing connections to various databases.
+    /// </summary>
+    public class ConnectUtil
+    {
+        /// <summary>
+        /// Connection method (connection creation) to MySql database.
+        /// </summary>
+        /// <returns>MySql connection</returns>
+        public static MySqlConnection GetConnection()
+        {
+            string connectionString = "Server=localhost;DataBase=trains;UId=root,Pwd=Hell.2002";
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
