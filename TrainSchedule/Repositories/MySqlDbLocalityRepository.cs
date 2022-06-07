bc1946c5b858c05c74b0de2e32456648c32211ea
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-07 11:45:51 +0300

    Implemented Append method in MySqlDbLocalityRepository class.
    Implemented a method for adding rows to the localities table.

--- TrainSchedule/Repositories/MySqlDbLocalityRepository.cs
+++ TrainSchedule/Repositories/MySqlDbLocalityRepository.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using TrainSchedule.Models;
+using TrainSchedule.Conections;
 using TrainSchedule.RepositoryInterfaces;
 
 namespace TrainSchedule.Repositories
@@ -12,7 +14,25 @@ namespace TrainSchedule.Repositories
     {
         public Locality Append(Locality newLocality)
         {
-            throw new NotImplementedException();
+            string sql = "INSERT INTO trains.localities (name_of_locality) VALUES @name_of_loc"; // Сформировать строку SQL-запроса на добавление данных в таблицу localities
+
+            using MySqlConnection connection = ConnectUtil.GetConnection(); // Создать соединение с БД MySql
+
+            connection.Open(); // Открыть соединение
+
+            try // Попытаться...
+            {
+                using MySqlCommand command = new MySqlCommand(sql, connection); // Сформировать готовый SQL-запрос на добавление в таблицу localities
+                command.Parameters.AddWithValue("@name_of_loc", newLocality.Name); // Присвоить значения параметров SQL-запросу
+                command.ExecuteNonQuery(); // Выполнить SQL-запрос
+                long localityId = command.LastInsertedId; // Узнать id объекта, который он получил в таблице БД
+                newLocality.Id = localityId; // Присвоить этот id экземпляру этого объекта внутри приложения
+                return newLocality; // Вернуть добавленный объект
+            }
+            catch (MySqlException exception) // Иначе... 
+            {
+                throw new RepositoryException(exception.ErrorCode, exception.Message); // Сообщить об ошибке
+            }
         }
 
         public Locality Update(Locality locality)
@@ -30,7 +50,7 @@ namespace TrainSchedule.Repositories
 
         }
 
-        public Locality GetById(uint id)
+        public Locality GetById(long id)
         {
             throw new NotImplementedException();
         }
