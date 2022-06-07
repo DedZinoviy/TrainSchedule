a4784dfa19e91dc2db7e8b637914f0fa3234ed65
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-07 14:31:49 +0300

    Added interface to interact with Passengers tables in databases.

---
+++ TrainSchedule/RepositoryInterfaces/IPassengerRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainSchedule.Models;
+
+namespace TrainSchedule.RepositoryInterfaces
+{
+    public interface IPassengerRepository
+    {
+        /// <summary>
+        /// Interface for Locality repositories.
+        /// </summary>
+        interface ILocalityRepository : IRepository<Passenger>
+        {
+            /// <summary>
+            /// Allows to update locality object in database.
+            /// </summary>
+            /// <param name="locality">Locality object to update.</param>
+            /// <returns>Updated locality object.</returns>
+            public Locality Update(Locality locality);
+
+            /// <summary>
+            /// Allows to append new locality object in database.
+            /// </summary>
+            /// <param name="newLocality">Locality object to append.</param>
+            /// <returns>Appended locality object</returns>
+            public Locality Append(Locality newLocality);
+
+            /// <summary>
+            /// Allows to delete locality object in database.
+            /// </summary>
+            /// <param name="locality">Locality object to delete.</param>
+            public void Delete(Locality locality);
+        }
+    }
+}
