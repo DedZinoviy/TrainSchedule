e9b8aa212eea7bf87e0f8cbaa983f94b83bd3bc7
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-18 10:40:49 +0300

    Implemented Passengers Browser in main form.
    The List Box has been replaced with a DataGridView, and passenger displays are loaded when the application is loaded.

--- TrainSchedule/Form1.Designer.cs
+++ TrainSchedule/Form1.Designer.cs
@@ -28,31 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.passengers = new System.Windows.Forms.ListBox();
             this.Add_pass = new System.Windows.Forms.Button();
             this.Delete_pass = new System.Windows.Forms.Button();
             this.Update_pass = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.passengers = new System.Windows.Forms.DataGridView();
+            this.last_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.first_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.patronim = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contacts = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.passengers)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // passengers
-            // 
-            this.passengers.FormattingEnabled = true;
-            this.passengers.HorizontalScrollbar = true;
-            this.passengers.ItemHeight = 15;
-            this.passengers.Location = new System.Drawing.Point(12, 12);
-            this.passengers.Name = "passengers";
-            this.passengers.Size = new System.Drawing.Size(371, 199);
-            this.passengers.TabIndex = 0;
-            this.passengers.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Add_pass
             // 
             this.Add_pass.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Add_pass.Location = new System.Drawing.Point(3, 3);
+            this.Add_pass.Location = new System.Drawing.Point(3, 4);
+            this.Add_pass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Add_pass.Name = "Add_pass";
-            this.Add_pass.Size = new System.Drawing.Size(118, 22);
+            this.Add_pass.Size = new System.Drawing.Size(164, 38);
             this.Add_pass.TabIndex = 1;
             this.Add_pass.Text = "Добавить";
             this.Add_pass.UseVisualStyleBackColor = true;
@@ -61,9 +59,10 @@
             // Delete_pass
             // 
             this.Delete_pass.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Delete_pass.Location = new System.Drawing.Point(127, 3);
+            this.Delete_pass.Location = new System.Drawing.Point(173, 4);
+            this.Delete_pass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Delete_pass.Name = "Delete_pass";
-            this.Delete_pass.Size = new System.Drawing.Size(117, 22);
+            this.Delete_pass.Size = new System.Drawing.Size(200, 38);
             this.Delete_pass.TabIndex = 2;
             this.Delete_pass.Text = "Удалить";
             this.Delete_pass.UseVisualStyleBackColor = true;
@@ -72,51 +71,132 @@
             // Update_pass
             // 
             this.Update_pass.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Update_pass.Location = new System.Drawing.Point(250, 3);
+            this.Update_pass.Location = new System.Drawing.Point(379, 4);
+            this.Update_pass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Update_pass.Name = "Update_pass";
-            this.Update_pass.Size = new System.Drawing.Size(118, 22);
+            this.Update_pass.Size = new System.Drawing.Size(174, 38);
             this.Update_pass.TabIndex = 3;
             this.Update_pass.Text = "Редактировать";
             this.Update_pass.UseVisualStyleBackColor = true;
-            this.Update_pass.Click += new System.EventHandler(this.button1_Click);
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.20921F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.79079F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 123F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.15419F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.84581F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 179F));
             this.tableLayoutPanel1.Controls.Add(this.Add_pass, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.Update_pass, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.Delete_pass, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 217);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 274);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(371, 28);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(556, 46);
             this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // passengers
+            // 
+            this.passengers.AllowUserToAddRows = false;
+            this.passengers.AllowUserToDeleteRows = false;
+            this.passengers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.passengers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.last_name,
+            this.first_name,
+            this.patronim,
+            this.contacts,
+            this.id});
+            this.passengers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passengers.Location = new System.Drawing.Point(3, 3);
+            this.passengers.MultiSelect = false;
+            this.passengers.Name = "passengers";
+            this.passengers.RowHeadersVisible = false;
+            this.passengers.RowHeadersWidth = 51;
+            this.passengers.RowTemplate.Height = 29;
+            this.passengers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.passengers.Size = new System.Drawing.Size(556, 264);
+            this.passengers.TabIndex = 5;
+            // 
+            // last_name
+            // 
+            this.last_name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.last_name.HeaderText = "Фамилия";
+            this.last_name.MinimumWidth = 6;
+            this.last_name.Name = "last_name";
+            // 
+            // first_name
+            // 
+            this.first_name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.first_name.HeaderText = "Имя";
+            this.first_name.MinimumWidth = 6;
+            this.first_name.Name = "first_name";
+            // 
+            // patronim
+            // 
+            this.patronim.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.patronim.HeaderText = "Отчество";
+            this.patronim.MinimumWidth = 6;
+            this.patronim.Name = "patronim";
+            // 
+            // contacts
+            // 
+            this.contacts.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.contacts.HeaderText = "Контакты";
+            this.contacts.MinimumWidth = 6;
+            this.contacts.Name = "contacts";
+            // 
+            // id
+            // 
+            this.id.HeaderText = "";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Visible = false;
+            this.id.Width = 125;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.passengers, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(180, 126);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(562, 324);
+            this.tableLayoutPanel2.TabIndex = 6;
             // 
             // TrainSchedule
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.passengers);
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "TrainSchedule";
             this.Text = "Train Schedule";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.passengers)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private ListBox passengers;
         private Button Add_pass;
         private Button Delete_pass;
         private Button Update_pass;
         private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView passengers;
+        private TableLayoutPanel tableLayoutPanel2;
+        private DataGridViewTextBoxColumn last_name;
+        private DataGridViewTextBoxColumn first_name;
+        private DataGridViewTextBoxColumn patronim;
+        private DataGridViewTextBoxColumn contacts;
+        private DataGridViewTextBoxColumn id;
     }
 }
--- TrainSchedule/Form1.cs
+++ TrainSchedule/Form1.cs
@@ -1,3 +1,8 @@
+using TrainSchedule.Services;
+using TrainSchedule.Models;
+using TrainSchedule.Repositories;
+using TrainSchedule.RepositoryInterfaces;
+
 namespace TrainSchedule
 {
     public partial class TrainSchedule : Form
@@ -7,19 +12,15 @@ namespace TrainSchedule
             InitializeComponent();
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+            IPassengerRepository rep = new MySqlDbPassangerRepository();
+            IEnumerable<Passenger> passes = rep.GetAll();
+            foreach (Passenger passenger in passes)
+            {
+                this.passengers.Rows.Add(passenger.LastName, passenger.FirstName, passenger.Patronim, passenger.Contacts, passenger.Id);
+            }
+            this.passengers.Sort(last_name, System.ComponentModel.ListSortDirection.Ascending);
         }
 
         private void Delete_pass_Click(object sender, EventArgs e)
