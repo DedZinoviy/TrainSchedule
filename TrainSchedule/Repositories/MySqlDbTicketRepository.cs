2a96b0a74b34798edf1b7becb2043f8f3be799bb
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-19 19:58:40 +0300

    Started the implementation of the Ticket repository for MySQL .

---
+++ TrainSchedule/Repositories/MySqlDbTicketRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainSchedule.Models;
+using TrainSchedule.RepositoryInterfaces;
+
+namespace TrainSchedule.Repositories
+{
+    public class MySqlDbTicketRepository : ITicketRepository
+    {
+        public Ticket Append(Ticket newTicket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket Update(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Ticket ticket)
+        {
+
+        }
+
+        public IEnumerable<Ticket> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetByPassenger(Passenger passenger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetByTrainNumber(long trainNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
