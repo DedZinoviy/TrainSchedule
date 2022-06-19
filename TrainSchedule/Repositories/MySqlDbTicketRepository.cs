96e70a09373237fefd12800cea2ea716c4b30f14
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-19 23:35:51 +0300

    Implemented GetAll method for MySql tickets reposiitory.

--- TrainSchedule/Repositories/MySqlDbTicketRepository.cs
+++ TrainSchedule/Repositories/MySqlDbTicketRepository.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using TrainSchedule.Models;
 using TrainSchedule.RepositoryInterfaces;
+using TrainSchedule.Conections;
 
 namespace TrainSchedule.Repositories
 {
@@ -27,7 +29,32 @@ namespace TrainSchedule.Repositories
 
         public IEnumerable<Ticket> GetAll()
         {
-            throw new NotImplementedException();
+            List<Ticket> tickets = new List<Ticket>(); // Считать, что изначально в БД нет пассажиров.
+            string query = @"SELECT * FROM trains.tickets"; // Сформировать строку запроса на получение списка пассажиров.
+            using MySqlConnection connection = ConnectUtil.GetConnection(); // Создать соединение с БД.
+            connection.Open(); // Открыть соединение
+            try // Попытаться...
+            {
+                using MySqlCommand command = new MySqlCommand(query, connection); // Сформировать запрос на получение списка пассажиров.
+                using MySqlDataReader reader = command.ExecuteReader();  // Выплнить запрос
+                while (reader.Read()) // Пока имеются полученные объекты...
+                {
+                    long idPassenger = reader.GetInt32(0); // Получить данные о пассажирах.
+                    int availability = reader.GetInt32(1);
+                    double cost = reader.GetDouble(2);
+                    long review_id = reader.GetInt32(3);
+                    long passenger_id = reader.GetInt32(4);
+                    long seat_id = reader.GetInt32(5);
+                    long route_id = reader.GetInt32(6);
+
+                    tickets.Add(new Ticket(idPassenger, availability, cost, review_id, passenger_id, seat_id, route_id)); // Добавить сформированные объекты пассажиров в коллекцию.
+                }
+                return tickets; // Вернуть коллекцию.
+            }
+            catch (MySqlException ex) // Иначе...
+            {
+                throw new RepositoryException(ex.ErrorCode, ex.Message); // Вывести исключение.
+            }
         }
 
         public IEnumerable<Ticket> GetByPassenger(Passenger passenger)
