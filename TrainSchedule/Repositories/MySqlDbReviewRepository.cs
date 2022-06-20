ce1dcd53b69ce59d798f688ffd93bb3616f7aa5e
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-20 17:38:58 +0300

    Created Review repository for MySql DB.

---
+++ TrainSchedule/Repositories/MySqlDbReviewRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainSchedule.RepositoryInterfaces;
+using TrainSchedule.Models;
+
+namespace TrainSchedule.Repositories
+{
+    public class MySqlDbReviewRepository : IReviewRepository
+    {
+
+        public Review Append(Review newReview)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Review Update(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Delete(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Review GetByTicket(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public float GetAverageEvaluationByTrain(Train train)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
