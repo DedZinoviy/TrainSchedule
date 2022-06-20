da3e87691eb2041a24217ef879329610d25f9268
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-20 22:25:57 +0300

    Implemented Append review method in MySql review repository.

--- TrainSchedule/Repositories/MySqlDbReviewRepository.cs
+++ TrainSchedule/Repositories/MySqlDbReviewRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using TrainSchedule.Conections;
 using TrainSchedule.RepositoryInterfaces;
 using TrainSchedule.Models;
 
@@ -13,7 +15,28 @@ namespace TrainSchedule.Repositories
 
         public Review Append(Review newReview)
         {
-            throw new NotImplementedException();
+            string sql = "INSERT INTO trains.reviews (review_name,evaluation,comment) VALUES (@r_name, @ev, @com)"; // Сформировать строку SQL-запроса на добавление данных в таблицу reviews
+
+            using MySqlConnection connection = ConnectUtil.GetConnection(); // Создать соединение с БД MySql
+
+            connection.Open(); // Открыть соединение
+
+            try // Попытаться...
+            {
+                using MySqlCommand command = new MySqlCommand(sql, connection); // Сформировать готовый SQL-запрос на добавление в таблице reviews.
+                command.Parameters.AddWithValue("@r_name", newReview.Name); // Присвоить значения параметров SQL-запросу
+                command.Parameters.AddWithValue("@ev", newReview.Evaluation);
+                command.Parameters.AddWithValue("@com", newReview.Description);
+
+                command.ExecuteNonQuery(); // Выполнить SQL-запрос
+                long localityId = command.LastInsertedId; // Узнать id объекта, который он получил в таблице БД
+                newReview.Id = localityId; // Присвоить этот id экземпляру этого объекта внутри приложения
+                return newReview; // Вернуть добавленный объект
+            }
+            catch (MySqlException exception) // Иначе... 
+            {
+                throw new RepositoryException(exception.ErrorCode, exception.Message); // Сообщить об ошибке
+            }
         }
 
 
