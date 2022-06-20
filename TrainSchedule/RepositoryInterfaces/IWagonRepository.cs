4a61106d12762150a62c2b0a2f8b70af885cf828
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-20 18:38:44 +0300

    Added interface for wagons repositories in database.

---
+++ TrainSchedule/RepositoryInterfaces/IWagonRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainSchedule.Models;
+
+namespace TrainSchedule.RepositoryInterfaces
+{
+    /// <summary>
+    /// Interface for wagons repositories.
+    /// </summary>
+    public interface IWagonRepository
+    {
+        /// <summary>
+        /// Allows to get collection of wagons related to train.
+        /// </summary>
+        /// <param name="train">Related train.</param>
+        /// <returns>Collection of wagons.</returns>
+        public IEnumerable<Wagon> GetByTrain(Train train);
+    }
+}
