c69fad3b2404eb37bb03a76cda78133ec19d578a
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-20 20:10:33 +0300

    Added interface for seats repository.

---
+++ TrainSchedule/RepositoryInterfaces/ISeatRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainSchedule.Models;
+
+namespace TrainSchedule.RepositoryInterfaces
+{
+    /// <summary>
+    /// Interface for Seat repositories in databases.
+    /// </summary>
+    public interface ISeatRepository
+    {
+        /// <summary>
+        /// Allows to get collection of vacant seats in wagon.
+        /// </summary>
+        /// <param name="wagon">A wagon for searching.</param>
+        /// <returns>Collection of vacant seats.</returns>
+        public IEnumerable<Seat> GetFreeSeatByWagon(Wagon wagon);
+    }
+}
