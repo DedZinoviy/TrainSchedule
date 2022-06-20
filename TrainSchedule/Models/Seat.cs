0b075e0719b9bf1667d2ff9c740095fb1baa98a4
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-20 19:34:49 +0300

    Added class for describing seats.

---
+++ TrainSchedule/Models/Seat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainSchedule.Models
+{
+    /// <summary>
+    /// Class for describing seats (places) in database.
+    /// </summary>
+    public class Seat
+    {
+        /// <summary>
+        /// Seat's ID.
+        /// </summary>
+        public long Id { get; set; }
+        
+        /// <summary>
+        /// Seat's type.
+        /// </summary>
+        public string Type { get; set; }
+        
+        /// <summary>
+        /// Seat's number.
+        /// </summary>
+        public int Number { get; set; }
+        
+        /// <summary>
+        /// Wagon id where seat placed.
+        /// </summary>
+        public long WagonId { get; set; }
+
+        /// <summary>
+        /// Parametrized constructor.
+        /// </summary>
+        /// <param name="id">Id of seat to be created.</param>
+        /// <param name="type">Type of seat to be created.</param>
+        /// <param name="num">Number of seat t be created.</param>
+        /// <param name="wag_id">Id of wagon where seat is placed.</param>
+        public Seat(long id, string type, int num, long wag_id)
+        {
+            Id = id;
+            Type = type;
+            Number = num;
+            WagonId = wag_id;
+        }
+    }
+}
